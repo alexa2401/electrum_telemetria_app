d5cfce5ff896429c646221cf7cf4ca3d051aaf56
alexa2401 <a00836348@tec.mx>
2024-07-02 19:40:56 -0600

    Añadido de las columnas de la seccion media

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ import PySimpleGUI as psg
 
 screen_width, screen_height = psg.Window.get_screen_size() #Obtener el tamaño de la pantalla actual
 
+#Columnas dentro del mod_medio
+col1_mod_medio = psg.Column([[psg.Canvas(background_color="#2C73B9", size=(screen_width/3, screen_height/3))]])
+col2_mod_medio = psg.Column([[psg.Canvas(background_color="#1B5590", size=(screen_width/3, screen_height/3))]])
+col3_mod_medio = psg.Column([[psg.Canvas(background_color="#0B3A69", size=(screen_width/3, screen_height/3))]])
+
+#Filas dentro de la ventana
 mod_superior = [psg.Canvas(background_color="red", size=(screen_width, screen_height/3))]
-mod_medio = [psg.Canvas(background_color="blue", size=(screen_width, screen_height/3))]
+mod_medio = [col1_mod_medio, col2_mod_medio, col3_mod_medio]
 mod_inferior = [psg.Canvas(background_color="green", size=(screen_width, screen_height/3))]
 
 layout = [                                                  #Creacion y definido del layout

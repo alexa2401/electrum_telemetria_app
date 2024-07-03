21a241b41a4df6282a558895f61b14c6342d26e5
alexa2401 <a00836348@tec.mx>
2024-07-02 21:33:18 -0600

    Se intento añadir una imagen de fondo, sin embargo no se logro

--- main.py
+++ main.py
@@ -4,24 +4,24 @@ screen_width, screen_height = psg.Window.get_screen_size() #Obtener el tamaño d
 
 # Layout de las imágenes para cada columna
 col1_mod_medio_layout = [
-    [psg.Image(source="imgs/dog.png", size=(screen_width/3, screen_height/3))]
+    #[psg.Image(source="imgs/dog.png", size=(screen_width/3, screen_height/3))]
 ]
 col2_mod_medio_layout = [
-    [psg.Image(source="imgs/cat.png", size=(screen_width/3, screen_height/3))]
+    #[psg.Image(source="imgs/cat.png", size=(screen_width/3, screen_height/3))]
 ]
 col3_mod_medio_layout = [
-    [psg.Image(source="imgs/hamster.png", size=(screen_width/3, screen_height/3))]
+    #[psg.Image(source="imgs/hamster.png", size=(screen_width/3, screen_height/3))]
 ]
 
 # Definición de las columnas utilizando sg.Column
-col1_mod_medio_column = psg.Column(col1_mod_medio_layout, size=(screen_width/3, screen_height/3))
-col2_mod_medio_column = psg.Column(col2_mod_medio_layout, size=(screen_width/3, screen_height/3))
-col3_mod_medio_column = psg.Column(col3_mod_medio_layout, size=(screen_width/3, screen_height/3))
+col1_mod_medio_column = psg.Column(col1_mod_medio_layout, size=(screen_width/3, (screen_height/6)*4))
+col2_mod_medio_column = psg.Column(col2_mod_medio_layout, size=(screen_width/3, (screen_height/6)*4))
+col3_mod_medio_column = psg.Column(col3_mod_medio_layout, size=(screen_width/3, (screen_height/6)*4))
 
 # Definición de los frames para cada columna
-col1_mod_medio_frame = psg.Frame('', [[col1_mod_medio_column]], size=(screen_width/3, screen_height/3))
-col2_mod_medio_frame = psg.Frame('', [[col2_mod_medio_column]], size=(screen_width/3, screen_height/3))
-col3_mod_medio_frame = psg.Frame('', [[col3_mod_medio_column]], size=(screen_width/3, screen_height/3))
+col1_mod_medio_frame = psg.Frame('', [[col1_mod_medio_column]], size=(screen_width/3, (screen_height/6)*4))
+col2_mod_medio_frame = psg.Frame('', [[col2_mod_medio_column]], size=(screen_width/3, (screen_height/6)*4))
+col3_mod_medio_frame = psg.Frame('', [[col3_mod_medio_column]], size=(screen_width/3, (screen_height/6)*4))
 
 # Definición de los layouts de los módulos
 mod_superior_layout = []
@@ -31,9 +31,9 @@ mod_medio_layout = [
 mod_inferior_layout = []
 
 # Definición de los frames de los módulos
-mod_superior_frame = psg.Frame('', mod_superior_layout, size=(screen_width, screen_height/3))
-mod_medio_frame = psg.Frame('', mod_medio_layout, size=(screen_width, screen_height/3))
-mod_inferior_frame = psg.Frame('', mod_inferior_layout, size=(screen_width, screen_height/3))
+mod_superior_frame = psg.Frame('', mod_superior_layout, size=(screen_width, screen_height/6))
+mod_medio_frame = psg.Frame('', mod_medio_layout, size=(screen_width, (screen_height/6)*4))
+mod_inferior_frame = psg.Frame('', mod_inferior_layout, size=(screen_width, screen_height/6))
 
 # Definición del layout principal
 master_layout = [
@@ -44,7 +44,7 @@ master_layout = [
 
 # Definición del layout de la ventana
 layout = [
-    [psg.Frame('', master_layout, expand_x=True, expand_y=True)]
+    [psg.Frame('', master_layout, background_color=None ,expand_x=True, expand_y=True)]
 ]
 
 window = psg.Window("Electrum", layout, size=(screen_width, screen_height), resizable=True) #Creacion de la ventana
